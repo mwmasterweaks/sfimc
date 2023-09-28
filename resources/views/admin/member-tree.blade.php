8cac73d7b083f6425b32fe72749af5f717435509
unknown <raymunddh@gmail.com>
2023-09-29 00:44:51 +0800

    ul li style

--- resources/views/admin/member-tree.blade.php
+++ resources/views/admin/member-tree.blade.php
@@ -17,35 +17,27 @@
 		<div class="row">
 			<div class="col-xs-12">
 	          	<div class="box box-success">
-					
 		            <div class="col-md-6">
-					  <ul>
-							{{-- @foreach($upline as $key => $upline)
-							  <li>
-								{{ "Level ".$upline->depth }} 
-								  	<ul>
-										<li>
-											{{ $upline->EntryCode }} - {{ ucwords(strtolower($upline->FirstName)) }} {{ ucwords(strtolower($upline->LastName)) }}
-										</li>
-									</ul>
-							  </li>
-						  	@endforeach --}}
-						  	@foreach($downline as $key => $downline)
-							  <li>
-								{{ "Level ".$key }} 
-								  	<ul>
-										@foreach($downline as $down)
-										<li>
-											{{ $down->EntryCode }} - {{ ucwords(strtolower($down->FirstName)) }} {{ ucwords(strtolower($down->LastName)) }}
-										</li>
-										@endforeach
-									</ul>
-							  </li>
-						  	@endforeach
-					  </ul>
+						{{-- @foreach($upline as $key => $upline)
+							<strong>{{ "Level ".$upline->depth }}</strong>
+							  <ul style="list-style-type:square;">
+								<li>
+									{{ $upline->EntryCode }} - {{ ucwords(strtolower($upline->FirstName)) }} {{ ucwords(strtolower($upline->LastName)) }}
+								</li>
+							</ul>
+						@endforeach --}}
+						
+						@foreach($downline as $key => $downline)
+							<strong>{{ "Level ".$key }} </strong>
+								<ul style="list-style-type:square;">
+									@foreach($downline as $down)
+									<li>
+										{{ $down->EntryCode }} - {{ ucwords(strtolower($down->FirstName)) }} {{ ucwords(strtolower($down->LastName)) }}
+									</li>
+									@endforeach
+								</ul>
+						@endforeach
 					</div>
-
-
           		</div>
           	</div>
 		</div>
